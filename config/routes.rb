723a8303a126a24a109930dd750d3d77dca8a42d
Rio Sanka <rio@RionoMacBook.local>
2019-08-07 20:29:47 +0900

    ajax form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'practice/index'
+  post 'practice/index'
 end
